2112c952b69e83ad974a4207ea7be925e5f4ff51
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2022-02-01 19:35:10 +0530

    Add "DisableKeyboards"
    New Block DisableKeyboards to disable the keyboard for all the textboxes in the activity.

--- src/io/mohamed/hidesystemkeyboard/HideSystemKeyboard.java
+++ src/io/mohamed/hidesystemkeyboard/HideSystemKeyboard.java
@@ -2,6 +2,9 @@ package io.mohamed.hidesystemkeyboard;
 
 import android.os.Build.VERSION;
 import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
@@ -29,12 +32,47 @@ public final class HideSystemKeyboard extends AndroidNonvisibleComponent {
    */
   @SimpleFunction(
       description = "Disables the system keyboard for the specified textbox, disallows the keyboard to show up when the textbox it clicked.")
-  public final void DisableSystemKeyboard(@NotNull TextBoxBase textbox) {
+  public void DisableSystemKeyboard(@NotNull TextBoxBase textbox) {
     EditText et = (EditText) textbox.getView();
+    disableEditTextKeyboard(et);
+  }
+
+  /**
+   * Disables the system keyboard for all the textboxes, same as DisableSystemKeyboard function
+   */
+
+  @SimpleFunction(
+      description = "Disables the keyboard for all the text boxes in the app")
+  public void DisableKeyboards() {
+    View view = form.getWindow().getDecorView();
+    if (!(view instanceof ViewGroup)) {
+      Log.e("HideSystemKeyboard", "Something went wrong");
+      return;
+    }
+    disableAllEditTextKeyboards((ViewGroup) view);
+  }
+
+  private void disableAllEditTextKeyboards(ViewGroup group) {
+    for (int i = 0; i < group.getChildCount(); i++) {
+      View childView = group.getChildAt(i);
+      if (childView instanceof ViewGroup) {
+        disableAllEditTextKeyboards((ViewGroup) childView);
+      } else if (childView instanceof EditText) {
+        disableEditTextKeyboard((EditText) childView);
+      }
+    }
+  }
+
+  private void disableEditTextKeyboard(EditText et) {
     if (VERSION.SDK_INT >= 21) {
       et.setShowSoftInputOnFocus(false);
     } else {
       et.setInputType(InputType.TYPE_NULL);
+    }
+    if (et.hasFocus()) {
+      // since the EditText has focus,
+      // the keyboard might be active, so hide it
+      form.HideKeyboard();
     }
   }
 
@@ -48,7 +86,7 @@ public final class HideSystemKeyboard extends AndroidNonvisibleComponent {
   @SimpleFunction(
       description = "Re-enables the system keyboard for the specified textbox, allows the keyboard to show up again when the textbox it clicked."
   )
-  public final void EnableSystemKeyboard(@NotNull TextBoxBase textbox) {
+  public void EnableSystemKeyboard(@NotNull TextBoxBase textbox) {
     EditText et = (EditText) textbox.getView();
     if (VERSION.SDK_INT >= 21) {
       et.setShowSoftInputOnFocus(true);
